f10af8465a1d30d445bc178c7d5e8b3c99bb5824
mguzman14 <mariaguzman1498@gmail.com>
2025-03-17 22:28:05 +0100

    feat(roulette): add technique inverse labrouchere (only to bet at even values)

--- shell/shared/roulette.sh
+++ shell/shared/roulette.sh
@@ -103,6 +103,200 @@ function martingala() {
 }
 
 
+function inverselab() {
+
+  echo -e "Dinero inicial: $money"
+  echo -n "A qué deseas apostar contínuamente (par/impar): " && read par_impar
+
+  declare -a mySeq=(1 2 3 4)
+
+  echo -e "\n[+] Empezamos con la secuencia [${mySeq[@]}]"
+
+  bet=$((${mySeq[0]} + ${mySeq[-1]}))
+  #money=$(($money - $bet))
+
+  mySeq=(${mySeq[@]})
+
+  #echo -e"\n[+] Invertimos $bet pavos, tenemos $money y la secuencia se queda en [${mySeq[@]}]"
+
+  jugadas_totales=0
+  bet_to_renew=$(($money+50)) # dinero que, una vez alcanzado, hará que renovemos la secuencia del 1 2 3 4
+
+  echo -e "El tope para actualizar la secuencia es a partir de $bet_to_renew pavos"
+
+  while [ ! $(($money-$bet)) -lt 0 ]; do
+
+    if [ $money -gt $bet_to_renew ]; then
+
+      echo -e "[!] Has superado el límite de dinero ($bet_to_renew pavos) para renovar la secuencia"
+
+      let bet_to_renew+=50
+
+      echo -e "El tope se ha actualizado a $bet_to_renew"
+
+      mySeq=(1 2 3 4)
+      bet=$((${mySeq[0]} + ${mySeq[-1]}))
+
+      echo -e "La secuencia se ha reestablecido a: [${mySeq[@]}]"
+
+    fi
+    
+    let jugadas_totales+=1
+    money=$(($money - $bet))
+ 
+    echo -e "\n[+] Invertimos $bet y nos quedamos con $money la secuencia para esta tirada es [${mySeq[@]}]"
+
+    random_number=$(($RANDOM % 37))
+
+
+
+    #echo -e "Ha salido el $random_number"
+
+
+    if [ "$par_impar" == "par" ]; then
+
+      if [ "$(($random_number % 2))" -eq 0 ] && [ "$random_number" -ne 0 ]; then
+
+        echo "ha salido el $random_number -> par -> ganas"
+
+        reward=$(($bet*2))
+        let money+=$reward
+
+        echo "habias apostado $bet y, al ganar, ahora tienes $money pavos"
+
+        if [ $money -gt $bet_to_renew ]; then
+
+          echo -e "[!] Has superado el límite de dinero ($bet_to_renew pavos) para renovar la secuencia"
+
+          let bet_to_renew+=50
+
+          echo -e "El tope se ha actualizado a $bet_to_renew"
+
+          mySeq=(1 2 3 4)
+          bet=$((${mySeq[0]} + ${mySeq[-1]}))
+
+          echo -e "La secuencia se ha reestablecido a: [${mySeq[@]}]"
+
+        elif [ $money -lt $(($bet_to_renew-100)) ]; then
+          
+          let bet_to_renew-=50 
+          echo -e "Se ha llegado al mínimo crítico -> reajustamos el tope a $bet_to_renew"
+          
+          mySeq+=($bet)
+          mySeq=(${mySeq[@]})
+
+          echo -e "Nuestra nueva secuencia es ${mySeq[@]}"
+
+          if [ "${#mySeq[@]}" -ne 1 ] && [ "${#mySeq[@]}" -ne 0 ]; then
+            bet=$((${mySeq[0]} + ${mySeq[-1]}))
+          elif [ "${#mySeq[@]}" -eq 1 ]; then # cuando haya un único elemento en mySeq
+            bet=${mySeq[0]}
+          else
+            echo "[!] La secuencia se ha quedado sin valores"
+            mySeq=(1 2 3 4)
+            echo -e "Reestablecemos la secuencia en [${mySeq}]"
+            bet=$((${mySeq[0]} + ${mySeq[-1]}))
+
+          fi
+
+
+        else
+          
+          mySeq+=($bet)
+          mySeq=(${mySeq[@]})
+
+          echo -e "Nuestra nueva secuencia es ${mySeq[@]}"
+
+          if [ "${#mySeq[@]}" -ne 1 ] && [ "${#mySeq[@]}" -ne 0 ]; then
+            bet=$((${mySeq[0]} + ${mySeq[-1]}))
+          elif [ "${#mySeq[@]}" -eq 1 ]; then # cuando haya un único elemento en mySeq
+            bet=${mySeq[0]}
+          else
+            echo "[!] La secuencia se ha quedado sin valores"
+            mySeq=(1 2 3 4)
+            echo -e "Reestablecemos la secuencia en [${mySeq}]"
+            bet=$((${mySeq[0]} + ${mySeq[-1]}))
+
+          fi
+
+        fi
+
+
+        # set new values
+        mySeq+=($bet)
+        mySeq=(${mySeq[@]})
+
+        # echo -e "La nueva secuencia es [${mySeq[@]}]"
+
+        if [ "${#mySeq[@]}" -ne 1 ] && [ "${#mySeq[@]}" -ne 0 ]; then
+          bet=$((${mySeq[0]} + ${mySeq[-1]}))
+        elif [ "${#mySeq[@]}" -eq 1 ]; then # cuando haya un único elemento en mySeq
+          bet=${mySeq[0]}
+        else
+          echo "[!] La secuencia se ha quedado sin valores"
+          mySeq=(1 2 3 4)
+          echo -e "Reestablecemos la secuencia en [${mySeq}]"
+          bet=$((${mySeq[0]} + ${mySeq[-1]}))
+
+        fi
+
+      elif [ "$random_number" -eq 0 ]; then
+
+        echo -e "Ha salido el $random_number -> pierdes"
+
+      elif [ "$((random_number % 2))" -eq 1 ] || [ "$random_number" -eq 0 ]; then
+
+        if [ "$((random_number % 2))" -eq 1 ]; then
+          echo -e "El numero es impar -> pierdes"
+
+        else
+          echo -e "El numero es 0 -> pierdes"
+
+        fi
+
+        echo "Ha salido el $random_number -> impar -> pierdes"
+
+        unset mySeq[0]
+        unset mySeq[-1] 2>/dev/null # si el array tiene 1 elemento, esto generará un error -> lo redirigimos al dev/null
+        mySeq=(${mySeq[@]})
+
+
+        echo -e "La secuencia que nos queda es: [${mySeq[@]}]"
+
+        if [ "${#mySeq[@]}" -ne 1 ] && [ "${#mySeq[@]}" -ne 0 ]; then
+          bet=$((${mySeq[0]} + ${mySeq[-1]}))
+        elif [ "${#mySeq[@]}" -eq 1 ]; then # cuando haya un único elemento en mySeq
+          bet=${mySeq[0]}
+        else
+          echo "[!] La secuencia se ha quedado sin valores"
+          mySeq=(1 2 3 4)
+          echo -e "Reestablecemos la secuencia en [${mySeq[@]}]"
+          bet=$((${mySeq[0]} + ${mySeq[-1]}))
+
+        fi
+
+
+
+
+        #bet=$((${mySeq[0]} + ${mySeq[-1]}))
+
+      fi
+
+    fi
+  #sleep 1
+  done
+
+  if [ "$money" -lt 0 ]; then
+
+    echo "Te has quedado sin pasta. Se acabó"
+    echo -e "\nEn total, han habido $jugadas_totales jugadas totales"
+    exit 1
+  fi
+
+
+}
+
+
 while getopts "m:t:h" arg; do
 	case $arg in
 		m) money=$OPTARG;;
@@ -116,7 +310,7 @@ if [ $money ] && [ $technique ]; then
 	if [ $technique == "marting" ]; then
 		martingala
 	elif [ $technique == "inverselab" ]; then
-		inverseLab
+		inverselab
 	else
 		echo -e "\n [!] Técnica no disponible."
   fi
